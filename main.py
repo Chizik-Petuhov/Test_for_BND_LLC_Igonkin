e6b6ea6f6c27a0ae9ab445c6bfc440b2cf966895
asyamart <asyamart132@yandex.ru>
2024-10-22 02:12:26 +0300

    commit

---
+++ main.py
@@ -0,0 +1,61 @@
+from ultralytics import YOLO
+import cv2
+import torch
+import supervision as sv
+import numpy as np
+from patched_yolo_infer import MakeCropsDetectThem, CombineDetections
+
+def recognise(input_video_path: str, output_video_path: str):
+    '''Берет видео по ссылке input_video_path, выделяет людей боксами и сохраняет видео в output_video_path'''
+
+
+    # Создаём покадровый считыватель видео
+    cap = cv2.VideoCapture(input_video_path)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Создаём объект для записи видео
+    output_path = output_video_path
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Разбиваем кадр на сегменты со сдвином
+        element_crops = MakeCropsDetectThem(
+            image=frame,
+            model_path="yolo11n.pt",
+            segment=False,
+            shape_x=640,
+            shape_y=640,
+            overlap_x=50,
+            classes_list=[0],
+            overlap_y=50,
+            conf=0.5,
+            iou=0.7,
+        )
+        # Находим на сегментах людей и обединяем их обратно в 1 кадр
+        result = CombineDetections(element_crops, nms_threshold=0.25)
+
+        #Отрисовываем людей
+        box = result.detected_xyxy_list_full
+        if len(box) != 0:
+            for param in box:
+                cv2.rectangle(frame, (param[0], param[1]), (param[2], param[3]), (255, 0, 0), 2)
+                font = cv2.FONT_HERSHEY_COMPLEX
+        out.write(frame)
+
+        #На всякий случай выход из программы
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    recognise("crowd.mp4", "output.mp4")
